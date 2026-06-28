87bbff96dff903fc14b7f2038a6a8d73edef2a00
xarlys32 <57732203+xarlys32@users.noreply.github.com>
2026-06-28 11:47:20 +0200

    Add customer contract

--- src/main/java/com/food/ordering/system/customer/contracts/CustomerQueryService.java
+++ src/main/java/com/food/ordering/system/customer/contracts/CustomerQueryService.java
@@ -1,4 +1,7 @@
 package com.food.ordering.system.customer.contracts;
 
+import com.food.ordering.system.shared.domain.valueobject.CustomerId;
+
 public interface CustomerQueryService {
+    boolean isValidUserId(CustomerId customerId);
 }
